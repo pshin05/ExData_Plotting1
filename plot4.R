1faba41a7abb41c1ba9eb61e4de9347bd1fe4360
pshin05 <pshin05@yahoo.com>
2015-05-10 07:08:48 -0700

    plot4 updated

--- plot4.R
+++ plot4.R
@@ -13,7 +13,7 @@ DateTime <- strptime(paste(Data2$Date, Data2$Time),
                      format="%d/%m/%Y %H:%M:%S")
 
 par(mfrow=c(2,2))
-par(mar=c(4,1,2,1))
+par(mar=c(4,4,2,2))
 par(oma=c(0,0,0,0))
 with(Data2, {
   plot(DateTime, 
@@ -28,12 +28,13 @@ with(Data2, {
        main="", xlab="", ylab="Energy sub metering")
   points(DateTime, Sub_metering_2, col="red", type="l")
   points(DateTime, Sub_metering_3, col="blue", type="l")
-  legend('topright', lty=c(1,1,1), col=c('black','red','blue'), box.lwd=0,
+  legend('topright', lty=c(1,1,1), col=c('black','red','blue'), 
+         box.lwd=0, cex=0.7,
          legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'))
   
   plot(DateTime, Global_reactive_power, type="l")
 })
 par(mfrow=c(1,1))
 
-dev.copy(png, 'plot4.png')
+dev.copy(png, file='plot4.png', width=480, height=480)
 dev.off()
